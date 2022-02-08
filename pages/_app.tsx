0a32845a99dae2e9e78cc45cace165d0a1ceb2ca
Conor Barnes <conorbarnes93@gmail.com>
2022-02-08 18:06:37 -0400

    next seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,33 @@ import "../public/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { withPasswordProtect } from "@storyofams/next-password-protect";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Head>
-				<meta name="viewport" content="viewport-fit=cover" />
-			</Head>
+			<DefaultSeo
+				openGraph={{
+					type: "website",
+					locale: "en_US",
+					url: "https://www.url.com/",
+					site_name: "SiteName",
+					images: [
+						{
+							url: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Tom_Thomson_-_The_Jack_Pine_1916.jpg",
+							alt: "Image description",
+							width: 2604,
+							height: 2380,
+						},
+					],
+				}}
+				twitter={{
+					handle: "@handle",
+					site: "@site",
+					cardType: "summary_large_image",
+				}}
+			/>
+
 			<Component {...pageProps} />
 		</>
 	);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,20 +10,7 @@ class MyDocument extends Document {
 		return (
 			<Html lang="en">
 				<Head>
-					{/* <meta charSet="UTF-8" /> */}
-					<meta name="twitter:card" content="summary" />
-					<meta property="og:locale" content="en_US" />
-					<meta property="og:type" content="website" />
-					<meta name="theme-color" content="#F61E61" />
-
-					<meta
-						property="og:image"
-						content="https://upload.wikimedia.org/wikipedia/commons/e/e6/Tom_Thomson_-_The_Jack_Pine_1916.jpg"
-					/>
-
-					<meta property="og:image:alt" content="Image description" />
-					<meta property="og:image:width" content="2604" />
-					<meta property="og:image:height" content="2380" />
+					<link rel="icon" href="/favicon.ico" />
 				</Head>
 				<body>
 					<Main />
